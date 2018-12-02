b2c76e4c151a28ba4ad6843b231e6391a3c6f378
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-12-02 22:33:47 +0000

    Reformatted day 1.2

--- day1/day1.2.py
+++ day1/day1.2.py
@@ -1,8 +1,11 @@
+import time
+
+
 def main():
-##    print('Test 1:', solve([+1, -1]))
-##    print('Test 2:', solve([+3, +3, +4, -2, -4]))
-##    print('Test 3:', solve([-6, +3, +8, +5, -6]))
-##    print('Test 4:', solve([+7, +7, -2, -7, -4]))
+    # print('Test 1:', solve([+1, -1]))
+    # print('Test 2:', solve([+3, +3, +4, -2, -4]))
+    # print('Test 3:', solve([-6, +3, +8, +5, -6]))
+    # print('Test 4:', solve([+7, +7, -2, -7, -4]))
     print('Calculating, please wait...')
     print('\nSolution:', solve(get_input()))
 
@@ -21,22 +24,21 @@ def solve(puzzle_input):
     current_sum = 0
     found_sums = [0]
     loops = 0
-    
+
     while True:
         loops += 1
         for i in puzzle_input:
             current_sum += i
-            
+
             if current_sum in found_sums:
                 return current_sum
             else:
                 found_sums.append(current_sum)
 
 
-import time
 start_time = time.time()
 main()
 end_time = time.time()
-print('\n' + ''.join(['-']*26) + '\nFINISHED\nTIME:', end_time - start_time,
-      '\n' + ''.join(['-']*26))
+print('\n' + ''.join(['-'] * 26) + '\nFINISHED\nTIME:', end_time - start_time,
+      '\n' + ''.join(['-'] * 26))
 input()
