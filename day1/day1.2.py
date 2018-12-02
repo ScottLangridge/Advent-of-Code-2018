76ee74d9209deb2bf6bada2d050722318ff9a879
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-12-02 19:09:54 +0000

    day 1 _ 2

---
+++ day1/day1.2.py
@@ -0,0 +1,35 @@
+def main():
+    print('Test 1:', solve([+1, -1]))
+    print('Test 2:', solve([+3, +3, +4, -2, -4]))
+    print('Test 3:', solve([-6, +3, +8, +5, -6]))
+    print('Test 4:', solve([+7, +7, -2, -7, -4]))
+    print('\nSolution:', solve(get_input()))
+
+
+def get_input():
+    out = []
+    with open('input.txt', 'r') as f:
+        raw = f.readlines()
+
+    for line in raw:
+        out.append(int(line.strip("\n")))
+    return out
+
+
+def solve(puzzle_input):
+    current_sum = 0
+    found_sums = [0]
+    loops = 0
+    
+    while True:
+        loops += 1
+        for i in puzzle_input:
+            current_sum += i
+            
+            if current_sum in found_sums:
+                return current_sum
+            else:
+                found_sums.append(current_sum)
+
+
+main()
