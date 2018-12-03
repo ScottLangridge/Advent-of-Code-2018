3361aef0d5ceb3c52e3fa5b404b00bedcedd72cd
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-12-03 15:09:41 +0000

    nice working solution

--- day3/day3.1.py
+++ day3/day3.1.py
@@ -1,3 +1,4 @@
+import sys
 import time
 
 from day3.Claim import Claim
@@ -22,9 +23,60 @@ def solve(puzzle_input):
     claims = []
     for claim_string in puzzle_input:
         claims.append(Claim(claim_string))
+    return get_overlap(claims)
 
 
+def get_overlap(claims):
+    if len(claims) == 0:
+        return 0
 
+    testing = claims[0]
+    overlap = 0
+    for claim in claims[1:]:
+        if claims_overlap(testing, claim):
+            overlap += measure_overlap(testing, claim)
+    return overlap + get_overlap(claims[1:])
+
+
+def claims_overlap(c1, c2):
+    for corner in c1.corners:
+        if c2.point_is_inside_claim(corner):
+            return True
+    else:
+        return False
+
+
+def measure_overlap(c1, c2):
+    if c1.top_left[0] < c2.top_left[0]:
+        start_x = c1.top_left[0]
+    else:
+        start_x = c2.top_left[0]
+    if c1.top_left[1] < c2.top_left[1]:
+        start_y = c1.top_left[1]
+    else:
+        start_y = c2.top_left[1]
+    if c1.bottom_right[0] > c2.bottom_right[0]:
+        end_x = c1.bottom_right[0]
+    else:
+        end_x = c2.bottom_right[0]
+    if c1.bottom_right[1] > c2.bottom_right[1]:
+        end_y = c1.bottom_right[1]
+    else:
+        end_y = c2.bottom_right[1]
+
+    x = start_x
+    overlap = 0
+    while x <= end_x:
+        y = start_y
+        while y <= end_y:
+            if c1.point_is_inside_claim((x,y)) and c2.point_is_inside_claim((x,y)):
+                overlap += 1
+            y += 1
+        x += 1
+    return overlap
+
+
+sys.setrecursionlimit(1500)
 start_time = time.time()
 main()
 end_time = time.time()
