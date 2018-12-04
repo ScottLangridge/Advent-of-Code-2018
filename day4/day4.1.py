53d97892573adaa01b9392636fe2121665ea6704
Scott Langridge <35850276+ScottLangridge@users.noreply.github.com>
2018-12-04 12:09:51 +0000

    created scrambled example input

--- day4/day4.1.py
+++ day4/day4.1.py
@@ -1,8 +1,9 @@
 import time
+from random import shuffle
 
 
 def main():
-    # print('Test:', solve(get_input('example1.txt')),'\n')
+    print('Test:', solve(get_input('example1.txt')),'\n')
     # print('Solution:', solve(get_input()))
     pass
 
@@ -20,8 +21,21 @@ def solve(puzzle_input):
     pass
 
 
+def debug():
+    pass
+
+
+def scramble_example():
+    with open('example1.txt', 'r') as f:
+        lines = f.readlines()
+        shuffle(lines)
+    with open('rand_example1.txt','w') as f:
+        f.writelines(lines)
+
+
 start_time = time.time()
-main()
+debug()
+# main()
 end_time = time.time()
 print('\n' + ''.join(['-'] * 26) + '\nFINISHED\nTIME:', end_time - start_time,
       '\n' + ''.join(['-'] * 26))
